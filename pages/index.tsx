2321895c298317cf94caefd0ceffe882c3300379
kushalsrinivas <kushal.s.2005@gmail.com>
2023-10-30 21:51:29 +0530

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,7 @@ import { data } from "@/courses";
 import { Input } from "@/components/ui/input";
 import Cards from "@/components/cards";
 import { ModeToggle } from "@/components/toogle-button";
-interface dataType {
-  course_name: string;
-  id: number;
-  platform: string;
-  free_course: string;
-  link: string;
-  difficulty: string;
-  module_number: number;
-  certification: string;
-}
+
 export default function Home() {
   const [input, setInput] = useState("");
   if (data) {
